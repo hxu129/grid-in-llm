3efe915ba6f59f1fb997e00e27b19bce557f4abc
Hua XU <hxu401@connect.hkust-gz.edu.cn>
2025-06-17 20:33:55 +0000

    Terminate model output

--- model.py
+++ model.py
@@ -353,5 +353,7 @@ class GPT(nn.Module):
             idx_next = torch.multinomial(probs, num_samples=1)
             # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
+            if idx_next == idx[:, 1]:
+                break
 
         return idx
